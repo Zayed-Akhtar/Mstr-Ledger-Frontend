549b5b4cf58cb710ab38016b089a292e8b754361
Zaid <zaid@Zaids-MacBook-Air.local>
2026-09-05 18:45:06 +0530

    Added PWA configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            manifest: {
+                name: 'Mstr-Ledger',
+                short_name: 'Mstr-Ledger',
+                description: 'Mstr-Ledger Application',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                start_url: '/',
+                scope: '/',
+
+                icons: [
+                    {
+                        src: '/mstr-ledger-desktop.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/mstr-ledger-desktop.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 })
